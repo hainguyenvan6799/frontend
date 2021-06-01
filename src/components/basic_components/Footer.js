cec3dee83cb3241052cb22d4e529ce0935fcbbee
hainguyenvan6799 <59387457+hainguyenvan6799@users.noreply.github.com>
2021-06-01 10:31:10 +0700

    Update Footer.js

--- src/components/basic_components/Footer.js
+++ src/components/basic_components/Footer.js
@@ -3,9 +3,6 @@ import React from 'react'
 function Footer() {
     return (
         <div>
-            <div>
-                <iframe title="display map" style={{ width: '100%' }} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.8564388348705!2d106.68526211477928!3d10.822296492290434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174deb3ef536f31%3A0x8b7bb8b7c956157b!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBDw7RuZyBuZ2hp4buHcCBUUC5IQ00!5e0!3m2!1svi!2s!4v1616057636677!5m2!1svi!2s" width={600} height={450} allowFullScreen loading="lazy" />
-            </div>
             <footer id="page-footer" className="py-3 bg-dark text-light">
                 <div id="footer">
                     <div className="footer-main">
